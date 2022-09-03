a1b0675d7c33f2b6a2638b6266bb63a6dd35fbd7
Patrick Dutra <patrickdutra100@hotmail.com>
2022-09-03 18:58:46 -0300

    fixed

--- index.js
+++ index.js
@@ -1,69 +1,70 @@
 //exercício 1
 
-const cards = document.querySelectorAll(".card")
+const cards = document.querySelectorAll(".card");
 
 cards.forEach((card) => {
-    cards.style.background = "ff6600"
-})
+    cards.style.background = "ff6600";
+});
 
 //exercício 2
 
-const headers = document.querySelectorAll(".titulo-card")
+const headers = document.querySelectorAll(".titulo-card");
 
 headers.forEach((header) => {
-    hearder.style.color = "#12265c"
-    header.innerHTML = "Meu Card"
-    header.style.margin = "8px"    
-})
+    hearder.style.color = "#12265c";
+    header.innerHTML = "Meu Card";
+    header.style.margin = "8px";
+});
 
 //exercício 4 e 5
 
-const descriptionCards = document.querySelectorAll("")
+const descriptionCards = document.querySelectorAll(".descriptionCards");
 
-descriptionCards.forEach(header) => 
-description.style.color = "white"
-description.textContent = "Descrição modificada"
-description.style.margin = "30px 0"  
+descriptionCards.forEach(function (btn) {
+    description.style.color = "white";
+    description.textContent = "Descrição modificada";
+    description.style.margin = "30px 0";
+});
 
 //exercicio 6
 
-const botaoEditar = document.querySelectorAll(".botao-editar")
+const botaoEditar = document.querySelectorAll(".botao-editar");
 
-//exercicio 7 
+//exercicio 7
 
-const botaoApagar = document.querySelectorAll(".botao-apagar")
+const botaoApagar = document.querySelectorAll(".botao-apagar");
 
 for (value of botaoApagar) {
-    value.style.padding = "10px"
-    value.style.color = "white"
-    value.style.borderRadius = "8px"
-    value.style.border = "none"
-    value.style.background = "red"
+    value.style.padding = "10px";
+    value.style.color = "white";
+    value.style.borderRadius = "8px";
+    value.style.border = "none";
+    value.style.background = "red";
 }
 
 //exercicio 8
 
-botaoEditar.forEach(botao =>{
-    botao.onclick = editarCard
-})
+botaoEditar.forEach((botao) => {
+    botao.onclick = editarCard;
+});
 
 function editarCard() {
-    alert("Clicou no botão editar")
+    alert("Clicou no botão editar");
 }
 
 //exercicio 9
 
 function apagarCard() {
-    let response = confirm("tem certeza da exclusão?")
+    let response = confirm("tem certeza da exclusão?");
     if (response) {
-        alert("confirmado!")
+        alert("confirmado!");
     } else {
-        alert("Cancelou!")
+        alert("Cancelou!");
     }
 }
 
-const variables = document.querySelectorAll(".botao-apagar")
+const variables = document.querySelectorAll(".botao-apagar");
 
 variables.forEach((element) => {
-    Element.setAtribute("onclick", "apagarCard()")
-})
+    Element.setAtribute("onclick", "apagarCard()");
+});
